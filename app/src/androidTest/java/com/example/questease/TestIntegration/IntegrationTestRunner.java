887f873e0a324b65d50c90dd1ab15a474b20f761
Aymeric Morin <aymericmorin59@gmail.com>
2024-11-26 11:21:27 +0100

    Test résolution IntegrationTestRunner

--- app/src/androidTest/java/com/example/questease/TestIntegration/IntegrationTestRunner.java
+++ app/src/androidTest/java/com/example/questease/TestIntegration/IntegrationTestRunner.java
@@ -1,4 +1,5 @@
 package com.example.questease.TestIntegration;
+import io.cucumber.junit.CucumberOptions;
 
 @CucumberOptions(glue = { "com.mytest.steps" }, tags = "~@wip" , features = { "features" })
 public class IntegrationTestRunner {
